fc5efae14657e94b9d7fff7515e1bcbf3b41dbea
6om1n <qhals2001@gmail.com>
2024-02-28 12:06:35 +0900

    이진 탐색 트리를 이용한 우선순위 큐 예제 구현

--- study/tree/binarySearchTree.c
+++ study/tree/binarySearchTree.c
@@ -1,79 +1,70 @@
 /*이진 탐색 트리를 이용한 우선순위 큐 구현
 이진탐색트리는 루트보다 작은 값이 왼쪽, 큰 값이 오른쪽에 넣는 방법이고
 inorder로 검색하면 오름차순으로 출력할 수 있다. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-//이진 탐색 트리 노드 구조체
 typedef struct TreeNode {
-    int data; 
+    int data;
     struct TreeNode *left;
     struct TreeNode *right;
 } treeNode;
 
-//이진 탐색 트리 구조체
 typedef struct {
     treeNode* root;
 } priorityQueue;
 
-//우선순위 큐 생성 함수
-priorityQueue* createPriorityQueue() {
-    priorityQueue* pq = (priorityQueue*)malloc(sizeof(priorityQueue));
-    pq -> root = NULL; //우선 생성될 때는 아무런 값도 가지고 있지 않는다.
-    return pq;
-}
-//노드 생성 함수
-treeNode* createNode(int data) {
-    treeNode* newNode = (treeNode *)malloc(sizeof(treeNode));
-    newNode -> data = data;
-    newNode -> left = newNode -> right = NULL;
+treeNode* createNode (int data) {
+    treeNode* newNode = (treeNode*)malloc(sizeof(treeNode));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
     return newNode;
 }
-//노드 삽입 함수 (재귀적)
+priorityQueue* createPriorityQueue() {
+    priorityQueue* pq = (priorityQueue*)malloc(sizeof(priorityQueue));
+    pq -> root = NULL;
+    return pq;
+}
 treeNode* insert(treeNode* root, int data) {
     if (root == NULL) {
         return createNode(data);
     }
-    if (data > root -> data) { 
-        root -> right = insert(root -> right, data);
+    if (data > root -> data) {
+        root -> right = insert(root->right, data);
     } else {
         root -> left = insert(root->left, data);
     }
 }
 void enqueue(priorityQueue* pq, int data) {
-    pq -> root = insert(pq -> root, data);
+    pq -> root = insert(pq->root, data);
 }
-/*삭제할 때는 내림차순으로 출략, 제일 큰 값부터 찾을 수 있도록*/
+//가장 큰 원소를 찾아서 삭제한다. 
 int dequeue(priorityQueue* pq) {
-    treeNode *current = pq -> root;
-    treeNode *parent = NULL;
-    /*삭제하려는 값이 없을 경우 예외처리*/
+    treeNode* current = pq -> root;
+    treeNode* parent = NULL;
     if (current == NULL) {
-        fprintf(stdout, "Queue is empty. Cannot dequeue. \n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Queue is empty. Cannot dequeue. \n");
+        exit (EXIT_FAILURE);
     }
-    while (current->right != NULL) {
+    //가장 오른쪽에 있는 값이 제일 크니까 오른쪽으로만 이동한다
+    while (current -> right != NULL) {
         parent = current;
         current = current -> right;
     }
-    int maxItem =  current -> data;
-    //부모가 NULL이 아니라는 말은 가장 큰 값이 루트가 아니라는 말과 같다.
-    //가장 큰 값을 삭제하고, 그 값에 왼쪽에 달린 값을 부모의 오른쪽에 연결해준다. 
+    int maxItem = current->data;
+    //제일 큰 값에 왼쪽에 있는 값이나, 널 값을 부모의 오른쪽에 옮긴다. 
     if (parent != NULL) {
-        parent-> right = current -> left;
-    }
-    //부모가 NULL이라는 것은 가장 큰 값이 루트노드에 있다는 말이다.
-    //즉, 왼쪽 서브트리만 있을 경우이므로, 루트를 왼쪽 서브트리로 바꿔준다.  
-    else {
+        parent -> right = current -> left;
+    } else {
         pq -> root = current -> left;
     }
     free(current);
     return maxItem;
-
 }
-void inorder (treeNode* root) {
+void inorder(treeNode* root) {
     if (root != NULL) {
-        inorder(root -> left);
+        inorder(root->left);
         printf("%d ", root -> data);
         inorder(root -> right);
     }
@@ -85,12 +76,12 @@ int main() {
     enqueue(pq, 19);
     enqueue(pq, 11);
     enqueue(pq, 96);
-    printf("Inorder traversal (ascending order): \n");
+    printf("Inorder traversal (ascending order:) \n");
     inorder(pq -> root);
-    printf("\nDequeue elements in descending order : \n");
+    printf("\nDequeue elements in descending order:\n");
     while (pq -> root != NULL) {
         int item = dequeue(pq);
-        printf("%d ", item);
+        printf("%d ",item);
     }
     free(pq);
     return 0;
